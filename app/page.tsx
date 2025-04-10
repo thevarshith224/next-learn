9c01226d84662fd798a53bcb211fe3b2c7cd7354
thevarshith224 <varshithk2577@gmail.com>
2025-04-10 09:25:06 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Welcome to 1K Products!</h1>
+    </main>
+  );
+}
+Add homepage

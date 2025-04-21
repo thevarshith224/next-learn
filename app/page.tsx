55f8f5e434b76f80d9f80ae3a8e7241e418612aa
thevarshith224 <varshithk2577@gmail.com>
2025-04-21 14:40:44 +0530

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
-      <h1>Welcome to 1K Products!</h1>
-    </main>
-  );
-}
